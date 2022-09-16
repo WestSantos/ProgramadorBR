e06b5261863a6470d6077cb7f24ff39f11a0c9b9
Weslley Teodoro dos Santos <94250413+WestSantos@users.noreply.github.com>
2022-09-16 17:23:09 -0300

    Add files via upload

--- Churrascometro/assets/js/main.js
+++ Churrascometro/assets/js/main.js
@@ -1,24 +1,53 @@
+
 function calc(){
-let adultos = parseInt(document.getElementById('adultos').value);
-let criancas = parseInt(document.getElementById('criancas').value);
-let horas = parseInt(document.getElementById('duracao').value);
-let carne = 0;
-let refrigerante = 0;
-let Cerveja = 0;
-let pessoas = adultos + (criancas*0.5);
-    if(horas<7){
-        carne = ((pessoas * ((400/6)*horas))/1000).toFixed(1);
-        refrigerante = Math.round((pessoas * ((1000/6)*horas))/2000);
-        Cerveja = Math.round((adultos * ((1200/6)*horas))/350);
+    document.getElementById("carne").innerHTML = numCarne() + `Kg de Carne`
+    document.getElementById("cerveja").innerHTML = numCerveja()+`L de Cerveja`
+    document.getElementById("bebidas").innerHTML = numRefri()+`L de Refrigerante`
+}
+
+function horasPessoas(){
+    let adultos = parseInt(document.getElementById('adultos').value);
+    let criancas = parseInt(document.getElementById('criancas').value);
+    let horas = parseInt(document.getElementById('duracao').value);
+    let numeroPessoas = adultos + (criancas*0.5);
+    return {numeroPessoas, criancas, horas, adultos};
+}
+
+function numCarne(){
+    let dados = horasPessoas();
+    let horas = dados.horas;
+    let pessoas = dados.numeroPessoas;
+    let resultCarne = 0;
+    if(horas >= 6){
+        resultCarne= pessoas * 650/1000;
     }else{
-        carne = ((pessoas * ((650/6)*horas))/1000).toFixed(1);
-        refrigerante = Math.round((pessoas * ((1500/6)*horas))/2000);
-        Cerveja = Math.round((adultos * ((2000/6)*horas)/350));   
-    }
-    console.log(carne +"Kg de Carne")
-    console.log(refrigerante +" Garrafas de 2L")
-    console.log(Cerveja+" Latas de Cerveja") 
-    document.getElementById("carne").innerHTML = carne + `Kg de Carne`
-    document.getElementById("cerveja").innerHTML = refrigerante +` Garrafas de 2L`
-    document.getElementById("bebidas").innerHTML = Cerveja+` Latas de Cerveja`
+        resultCarne = pessoas * 400/1000;
+    };
+    return resultCarne;
+}
+
+function numRefri(){
+    let dados = horasPessoas();
+    let horas = dados.horas;
+    let pessoas = dados.numeroPessoas;
+    let resultRefri = 0;
+    if(horas >= 6){
+        resultRefri= pessoas * 1500/1000;
+    }else{
+        resultRefri = pessoas * 1000/1000;
+    };
+    return resultRefri;
+}
+
+function numCerveja(){
+    let dados = horasPessoas();
+    let horas = dados.horas;
+    let adultos = dados.adultos;
+    let resutCerv = 0;
+    if (horas >= 6){
+        resutCerv = adultos * 2000/1000
+    }else{
+        resutCerv = adultos * 1200/1000
+    };
+    return resutCerv;
 }
